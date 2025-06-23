c22ca9c280d699cea6c62fe0273326a093661e99
Gabriel Vinicius <gabrielviniciusdecs@gmail.com>
2025-06-22 23:12:08 -0300

    Update script.js

--- New File/script.js
+++ New File/script.js
@@ -1,5 +1,7 @@
-  const menuToggle = document.querySelector('.menu-toggle');
-  const navMenu = document.querySelector('.nav-menu');
-  menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
+// Seleciona o botão do menu e o menu de navegação (alteração para funcionamento do menu responsivo)
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+// Adiciona/remover a classe 'active' para abrir/fechar o menu lateral (alteração para funcionamento do menu responsivo)
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
